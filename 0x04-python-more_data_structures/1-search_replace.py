989d216be1768722ff17d8da6be9094ecc310d59
Charlestimi <charlestimiebifemere@gmail.com>
2023-12-06 07:41:59 -0800

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-   """ new_list()
-    for element in my_list:
-        if element == search
-        my_list.append()
-        else:
-            my_list.append(new_list)
-          return new_list"""
-new_list = [replace if x == search else x for x in my_list]
+    return ([replace if x == search else x for x in my_list])
