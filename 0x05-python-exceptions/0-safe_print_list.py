e3435c05e250f296401e31a0347a0909f0a74601
Charlestimi <charlestimiebifemere@gmail.com>
2023-12-18 23:07:27 -0800

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[idn], end="")
             counter += 1
-        except:
+        except (Exception):
             break
     print()
     return counter
